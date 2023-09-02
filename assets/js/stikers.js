9934471ebe695a706b466ffd1cc723573dcf298b
Pamela Maturana <pamela.mv22@hotmail.com>
2023-09-02 19:17:23 -0400

    se crea desafio de stickers, se agregan imagen para fondo navbar y main. se construye css y js

---
+++ assets/js/stikers.js
@@ -0,0 +1,18 @@
+function metodoSticker() {
+  const inputSticker1 = document.querySelector("#input-sticker-1");
+  const inputSticker2 = document.querySelector("#input-sticker-2");
+  const inputSticker3 = document.querySelector("#input-sticker-3");
+
+  const total =
+    Number(inputSticker1.value) +
+    Number(inputSticker2.value) +
+    Number(inputSticker3.value);
+
+  const parrafo = document.querySelector("#parrafo");
+
+  if (Number(total) <= 10) {
+    parrafo.innerHTML = "Llevas " + total + " stickers";
+  } else {
+    parrafo.innerHTML = "Llevas demasiados stickers " + "(" + total + ")";
+  }
+}
